bcfe969bff34df9055595a41596e5490ee70c5a7
Julian Battaglino <julianbattaglino@gmail.com>
2022-09-15 22:22:43 -0300

    fixing aos.js animations bugs,

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+// Mobile menu navigation display none onclick submenu
+// $('.header .navigation .menu li a').click(function(e) {
+//         jQuery('.navigation').css('display', 'none');
+//     });
+
